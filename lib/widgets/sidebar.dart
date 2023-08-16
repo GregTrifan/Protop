541a70378cd55b2e43296bd22e8f2aaf95c703ea
Grigore-Gabriel Trifan <trifangaby71@protonmail.com>
2023-08-16 14:02:39 +0300

    fix shadows

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -63,7 +63,7 @@ class _SidebarWidgetState extends State<SidebarWidget>
                   borderRadius: const BorderRadius.all(Radius.circular(15)),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
+                      color: Colors.black87.withOpacity(0.5),
                       spreadRadius: 5,
                       blurRadius: 7,
                       offset: const Offset(0, 3), // changes position of shadow
